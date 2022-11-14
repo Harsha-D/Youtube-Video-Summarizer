9bab97351bf3774a698f30db200efb86886a16d4
Harsha-D <65390137+Harsha-D@users.noreply.github.com>
2022-11-14 11:42:14 +0530

    Removed functionality for small summary button

--- yt-summarizer-chrome-extension/popup.js
+++ yt-summarizer-chrome-extension/popup.js
@@ -1,4 +1,4 @@
-var url_yt,button1 = document.getElementById("summarybutton"),button2 = document.getElementById("summarybutton1"),button3 = document.getElementById("summarybutton2");
+var url_yt,button1 = document.getElementById("summarybutton1"),button2 = document.getElementById("summarybutton2");
 
 function generateSummary(url_yt,x) {
     var req = new XMLHttpRequest();
@@ -9,19 +9,14 @@ function generateSummary(url_yt,x) {
             document.getElementById("plswait").innerHTML = "";
             button1.style.display = 'initial';
             button2.style.display = 'initial';
-            button3.style.display = 'initial';
        }
     };  
     if(x==1){
-        req.open("GET", `http://localhost:5000/api1/summarize1?youtube_url=${url_yt}`, true);
+        req.open("GET", `http://localhost:5000/api/summarize1?youtube_url=${url_yt}`, true);
         req.send();
     }
     if(x==2){
-        req.open("GET", `http://localhost:5000/api2/summarize2?youtube_url=${url_yt}`, true);
-        req.send();
-    }
-    if(x==3){
-        req.open("GET", `http://localhost:5000/api3/summarize3?youtube_url=${url_yt}`, true);
+        req.open("GET", `http://localhost:5000/api/summarize2?youtube_url=${url_yt}`, true);
         req.send();
     }
     console.log("Running..");
@@ -33,30 +28,18 @@ button1.addEventListener("click", function() {
         document.getElementById("plswait").innerHTML = "Fetching summary, please wait...";
         button1.style.display = 'none';
         button2.style.display = 'none';
-        button3.style.display = 'none';
         var x=1;
         generateSummary(url_yt,x);
     });
 }, false);
+
 button2.addEventListener("click", function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
         url_yt = tabs[0].url;
         document.getElementById("plswait").innerHTML = "Fetching summary, please wait...";
         button1.style.display = 'none';
         button2.style.display = 'none';
-        button3.style.display = 'none';
         var x=2;
-        generateSummary(url_yt,x);
-    });
-}, false);
-button3.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        url_yt = tabs[0].url;
-        document.getElementById("plswait").innerHTML = "Fetching summary, please wait...";
-        button1.style.display = 'none';
-        button2.style.display = 'none';
-        button3.style.display = 'none';
-        var x=3;
         generateSummary(url_yt,x);
     });
 }, false);
