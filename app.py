e533b147f07cb05e27f878e5fdca0445bdf5c9e4
Harsha-D <65390137+Harsha-D@users.noreply.github.com>
2022-11-14 11:38:45 +0530

    Reduced number of routes to two

--- app.py
+++ app.py
@@ -28,21 +28,35 @@ def Transcript_To_Text(transcript):
         speech_text_string += " "
     return speech_text_string
 
-def Text_Summary_Small(script):
+def Text_Summary_Small_Tokenizer(script):
     model = T5ForConditionalGeneration.from_pretrained("t5-base")
     tokenizer = T5Tokenizer.from_pretrained("t5-base")
     inputs = tokenizer.encode("summarize: " + script, return_tensors="pt", max_length=512,truncation=True)
     outputs = model.generate(  
     inputs, 
-    max_length=300,
-    min_length= 100,
+    max_length=150,
+    min_length=50,
     length_penalty=2.0, 
     num_beams=4, 
     early_stopping=True)
     return tokenizer.decode(outputs[0])
 
+def Text_Summary_Small(script):
+    summarizer = pipeline('summarization', model="t5-base", truncation=True)
+    num_iters = int(len(script)/4000)
+    summarized_text = []
+    for i in range(0, num_iters + 1):
+        start = 0
+        start = i * 4000
+        end = (i + 1) * 4000
+        out = summarizer(script[start:end])
+        out = out[0]  
+        out = out['summary_text'] 
+        summarized_text.append(out)
+    return " ".join(summarized_text)
+
 def Text_Summary_Medium(script):
-    summarizer = pipeline('summarization', model="t5-base")
+    summarizer = pipeline('summarization', model="t5-base", truncation=True)
     num_iters = int(len(script)/2000)
     summarized_text = []
     for i in range(0, num_iters + 1):
@@ -56,7 +70,7 @@ def Text_Summary_Medium(script):
     return " ".join(summarized_text)
     
 def Text_Summary_Large(script):
-    summarizer = pipeline('summarization',model="t5-base")
+    summarizer = pipeline('summarization',model="t5-base", truncation=True)
     num_iters = int(len(script)/1000)
     summarized_text = []
     for i in range(0, num_iters + 1):
@@ -72,23 +86,9 @@ def Text_Summary_Large(script):
 @app.errorhandler(404)
 def handle_exception(e):
     return f'Bad Request! Error: {HTTPStatus(e.code)}, {HTTPStatus(e.code).phrase}'
-
-@app.route('/api1/summarize1', methods=['GET'])
+    
+@app.route('/api/summarize1', methods=['GET'])
 def api1():
-    url_yt = request.args.get('youtube_url', None)
-    if url_yt==None:
-        abort(404)
-    x = url_yt.split("v=")
-    v_id = str(x[1])
-    transcript = YouTubeTranscriptApi.get_transcript(v_id)
-    script = Transcript_To_Text(transcript)
-    resp = f.Response(Text_Summary_Small(script))
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    resp.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token'
-    return resp
-@app.route('/api2/summarize2', methods=['GET'])
-def api2():
     url_yt = request.args.get('youtube_url', None)
     if url_yt==None:
         abort(404)
@@ -101,8 +101,9 @@ def api2():
     resp.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     resp.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token'
     return resp
-@app.route('/api3/summarize3', methods=['GET'])
-def api3():
+
+@app.route('/api/summarize2', methods=['GET'])
+def api2():
     url_yt = request.args.get('youtube_url', None)
     if url_yt==None:
         abort(404)
